f3ee4283bd71bf88b384c830431d3cd0ea27c292
sachin bhandari <bhandari.sachin.fi@gmail.com>
2025-11-10 17:07:12 +0200

    Implement: Add morgan middleware for logging

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-
-const userRouter = require('./routes/userRouter');
-
+const userRouter = require("./routes/userRouter");
+const morgan = require("morgan");
 
 const express = require("express");
 const app = express();
@@ -9,8 +8,11 @@ const tourRoutes = require("./routes/tourRoutes.js");
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 
-app.use('/users', userRouter);
+app.use("/users", userRouter);
 
 // Use the tourRouter for all /tours routes
 app.use("/tours", tourRoutes);
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,12 +1,10 @@
 const User = require("../models/userModel.js");
 
-// Get /Tours
 const getAllUsers = (req, res) => {
   const users = User.getAll();
   res.status(200).json(users);
 };
 
-// Get /Tours/:tourId
 const getUserById = (req, res) => {
   const userId = req.params.userId;
   const user = User.findById(userId);
@@ -16,7 +14,7 @@ const getUserById = (req, res) => {
     res.status(404).json({ message: "User not found" });
   }
 };
-// create /users
+
 const createUser = (req, res) => {
   const newUser = User.addOne({ ...req.body });
   if (newUser) {
@@ -26,7 +24,6 @@ const createUser = (req, res) => {
   }
 };
 
-// Put /Tours/:tourId
 const updateUser = (req, res) => {
   const userId = req.params.userId;
   const updatedUser = User.updateOneById(userId, { ...req.body });
@@ -37,7 +34,6 @@ const updateUser = (req, res) => {
   }
 };
 
-// Delete /Tours/:tourId
 const deleteUser = (req, res) => {
   const userId = req.params.userId;
   const isDeleted = User.deleteOneById(userId);
