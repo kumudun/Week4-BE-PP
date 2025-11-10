79515d1d789b2d967662ef49ec670721d79eb5a7
sachin bhandari <bhandari.sachin.fi@gmail.com>
2025-11-10 20:15:17 +0200

    Implement Custom Middleware for Authentication

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth.js");
 
 const {
   getAllUsers,
@@ -9,16 +10,15 @@ const {
   deleteUser,
 } = require("../controllers/userControllers.js");
 
-// ROUTES
-
+// PUBLIC ROUTES
 router.get("/", getAllUsers);
-
-router.post("/", createUser);
-
 router.get("/:userId", getUserById);
 
-router.put("/:userId", updateUser);
+// PROTECTED ROUTES (require admin=true)
+router.use(auth);
 
+router.post("/", createUser);
+router.put("/:userId", updateUser);
 router.delete("/:userId", deleteUser);
 
 module.exports = router;
